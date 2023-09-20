b1471aee18510071d7e3cead08c74597036a9a38
robsthecreator <robson01227@gmail.com>
2023-09-20 19:43:35 -0300

    conteúdo criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Teremos um homem aranha 4 ?",
+        "description":"Depois do super sucesso de bilheteria, com “Homem-Aranha: Sem Volta Para Casa”, os fãs de um dos heróis favoritos do mundo todo, estão ansiosos para saber se haverá o Homem-Aranha 4. Ao que tudo indica, a Sony e Marvel Studios planejam a produção e estão esperando a confirmação dos protagonistas da trama para continuar a saga.",
+        "photoCover": "https://musicaecinema.com/wp-content/uploads/2023/01/homem-aranha-4-capa.jpeg"
+    },
+    {
+        "id":"2",
+        "title":"Disney anuncia segunda temporada de Loki",
+        "description":"blablabla",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+]
